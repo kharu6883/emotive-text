bee36f7b526b41288979726b423fa598b03fe69e
Haru <kharu6883@gmail.com>
2021-01-16 03:28:56 -0500

    Google Credentials

--- server/server.js
+++ server/server.js
@@ -4,23 +4,28 @@ const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
-// GOOGLE_APPLICATION_CREDENTIALS=E:\repos\emotive-text\server\credentials-69a069f361eb.json
-
 app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
 });
 
-async function quickStart() {
-    const vision = require('@google-cloud/vision');
+async function quickStart(fileName) {
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = "./credentials-69a069f361eb.json"
 
+    const vision = require('@google-cloud/vision');
     const client = new vision.ImageAnnotatorClient();
 
-    const [result] = await client.labelDetection('./test.jpg');
-    const labels = result.labelAnnotations;
-    console.log('Labels:');
-    labels.forEach(label => console.log(label.description));
+    const [result] = await client.faceDetection(fileName);
+    const faces = result.faceAnnotations;
+    console.log('Faces:');
+    faces.forEach((face, i) => {
+        console.log(`  Face #${i + 1}:`);
+        console.log(`    Joy: ${face.joyLikelihood}`);
+        console.log(`    Anger: ${face.angerLikelihood}`);
+        console.log(`    Sorrow: ${face.sorrowLikelihood}`);
+        console.log(`    Surprise: ${face.surpriseLikelihood}`);
+    });
 }
 
-quickStart();
+quickStart('./test.jpg');

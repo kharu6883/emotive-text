8e73146e7f391a4214adbbf507a04e5c3851d9d8
Haru <kharu6883@gmail.com>
2021-01-16 01:54:43 -0500

    Setup Express

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port: " + PORT);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});

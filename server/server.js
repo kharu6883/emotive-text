2e7f80777389b3f78db673c491ce7bf066066daa
Brandon Ngo <b6ngo@uwaterloo.ca>
2021-01-16 07:08:29 -0500

    speech to text api added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const router = express.Router();
 
 const PORT = 3000;
 
@@ -28,6 +29,40 @@ async function quickStart(fileName) {
     });
 }
 
+async function textToSpeech(fileName) {
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = "./credentials-69a069f361eb.json"
+
+    const speech = require('@google-cloud/speech');
+    const client = new speech.SpeechClient();
+
+    const file = require('fs').readFileSync(fileName);
+    const audioBytes = file.toString('base64');
+	
+	const audio = {
+		content: audioBytes
+	};
+	
+	const config = {
+		encoding: 'LINEAR16',
+		sampleRateHertz: 16000,
+		languageCode: 'en-US'
+	};
+	
+	const request = {
+		audio: audio,
+		config: config
+    };
+    
+    const [response] = await client.recognize(request);
+	
+    const transcription = response.results.map(result => 
+        result.alternatives[0].transcript).join('\n');
+
+    console.log(transcription);
+
+}
+
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
-})
+    res.send(textToSpeech('Welcome.wav'));
+});

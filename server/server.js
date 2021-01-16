e75111dbaf23145cd84f7a6c16113de1d98c1044
Haru <kharu6883@gmail.com>
2021-01-16 03:06:20 -0500

    Google Cloud Integration

--- server/server.js
+++ server/server.js
@@ -4,12 +4,23 @@ const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
+// GOOGLE_APPLICATION_CREDENTIALS=E:\repos\emotive-text\server\credentials-69a069f361eb.json
+
 app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+async function quickStart() {
+    const vision = require('@google-cloud/vision');
+
+    const client = new vision.ImageAnnotatorClient();
+
+    const [result] = await client.labelDetection('./test.jpg');
+    const labels = result.labelAnnotations;
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+}
+
+quickStart();

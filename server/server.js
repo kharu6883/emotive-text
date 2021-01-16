624841b7d5775ee163c0cb857729d7f7c360c448
Brandon Ngo <okibrandon@hotmail.com>
2021-01-16 18:10:47 -0500

    added get calls for emotion and transcription

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
 });
 
-async function quickStart(fileName) {
+async function getEmotion(fileName) {
     process.env.GOOGLE_APPLICATION_CREDENTIALS = "./credentials-69a069f361eb.json"
 
     const vision = require('@google-cloud/vision');
@@ -29,7 +29,7 @@ async function quickStart(fileName) {
     });
 }
 
-async function textToSpeech(fileName) {
+async function getSpeechText(fileName) {
     process.env.GOOGLE_APPLICATION_CREDENTIALS = "./credentials-69a069f361eb.json"
 
     const speech = require('@google-cloud/speech');
@@ -58,9 +58,22 @@ async function textToSpeech(fileName) {
     const transcription = response.results.map(result => 
         result.alternatives[0].transcript).join('\n');
 
-    console.log(transcription);
+    return transcription;
 
 }
+
+
+app.get('/get-emotion', (req, res, err)=> {
+    try {
+        return res.send(getEmotion(res.locals.picture));
+    } catch {console.log(err)};
+});
+
+app.get('/get-text', (req, res, err) => {
+    try {
+        return res.send(getSpeechText(res.locals.recording))
+    } catch {console.log(err)};
+});
 
 
 app.get('/', (req, res) => {

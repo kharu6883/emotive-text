93c4e1fd9e2442aca86334fcd5975b50a93227b7
Haru <kharu6883@gmail.com>
2021-01-16 01:56:35 -0500

    Body Parser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 const PORT = 3000;
+
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
